9f91322763b6a83051a2222d5d5d328deea15f7f
Mason Lennon <masonlen@ad.unc.edu>
2021-09-07 14:03:54 -0400

    good

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,11 @@
-# TODO: Write docstring here
+"this program asks users their name, then gives them some uplifting chat to help them get through the day"
 
-# TODO: Initialize __author__ variable here
+__author__ = "730401465"
 
-# TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print("You entered: ")
+print(name)
+print("hey "+(name)+", stop limiting yourself based on other people's opinions")
+print(name+" is a really cool name, I bet you're fun to be around")
+print("you look really good today "+(name)+"!")
+
